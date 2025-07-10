79763f5c1e44539344c47ace892e07c43bde727d
CORPORATE\inthei00 <indira.thevar@ingrammicro.com>
2025-07-10 12:51:59 +0530

    Day 1: Java basics practice

--- src/com/indira/basic/dsaproblems/DSA1.java
+++ src/com/indira/basic/dsaproblems/DSA1.java
@@ -1,9 +1,24 @@
 package com.indira.basic.dsaproblems;
 
+import java.util.Scanner;
+
 public class DSA1 {
+	
+	public static void printNumbers(int n) {
+		for(int i=0;i<=n;i++) {
+			System.out.println(i + " ");
+		}
+		
+	}
 
 	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter numbers");
+		int n=sc.nextInt();
+		printNumbers(n);
 
+		 sc.close();
 		System.out.println("Hello World");
 	}
 
